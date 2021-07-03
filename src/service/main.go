18bb6c0a095b7b66beeb14698dd8f383c3b3e0ad
Abszissex <rubSXkwZDZGqrr4>
2021-07-03 07:41:00 +0200

    Added some comments

--- src/service/main.go
+++ src/service/main.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"log"
+	// Import local 'models' module
 	"models"
 )
 
 func main() {
+	// Create new "MyEvent" struct
 	ev := new(models.MyEvent)
+
+	// Defining the properties of the struct
 	ev.Type = "My Type"
 	ev.SecondParam = "Another param"
+
+	// Print the "MyEvent" struxt to the console
 	log.Printf("Event %v", ev)
 
+	// Print the returned result of the "MyPublicFunc()"
 	log.Println(models.MyPublicFunc())
 }
